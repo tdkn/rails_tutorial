b7e258127c0254a2ee964310bc43b9ad04144656
tdkn <web@tedokon.com>
2014-03-08 20:15:27 +0900

    Initial commit

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '46288a668ee9d1a1efaf714870d7f755a800e0537f1f59a80a9c6ba80df2a59734c00becf4a47e6dd7d2228e685733d4ea8be71275fdd1a781ea185f003665a8'
---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '79bf1ee8de23367c26fccd47f84c853c19aa10ec29b2ec15454506bd86b43f5a0229015911bda40ac67a87f330e7e93d2a0135ecc54db8fbbeb41790dfd25115'
